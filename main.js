d702c51bde2d8c05e37764a64346799396349ab5
FlyingAnt <flying2010.liu@gmail.com>
2016-05-19 09:42:44 +0800

    demo

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import TinyAudioPlayer from './src/TinyAudioPlayer';
 
 ReactDOM.render(<TinyAudioPlayer
-    name='This is a test audio'
+    name='Metallica - Enter Sandman'
     source='https://upload.wikimedia.org/wikipedia/en/3/39/Metallica_-_Enter_Sandman.ogg'
     />, document.getElementById('root'));
